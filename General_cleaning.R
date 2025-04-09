70ea3b2e17500a0058ea246f4b20e9073f7ed4be
AndrewRT7 <119531852+AndrewRT7@users.noreply.github.com>
2025-04-10 01:49:23 +0300

    Fixing mistake after previous commit

--- General_cleaning.R
+++ General_cleaning.R
@@ -1,5 +1,7 @@
 library(tidyverse)
 
+movies_initial <- read_csv("TMDB_movie_dataset_v11.csv")
+
 month_translation <- c(
   Jan = "Січ", Feb = "Лют", Mar = "Бер", Apr = "Кві", May = "Тра", Jun = "Чер",
   Jul = "Лип", Aug = "Сер", Sep = "Вер", Oct = "Жов", Nov = "Лис", Dec = "Гру")
